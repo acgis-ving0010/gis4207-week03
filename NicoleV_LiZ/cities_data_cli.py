c4beea71622d5afd5cd007b434acaa25f64bbfdb
Li Zhuang <zhua0016@algonquinlive.com>
2025-01-22 22:50:56 -0500

    modify cities_data_cli.py and lauch.json

--- NicoleV_LiZ/cities_data_cli.py
+++ NicoleV_LiZ/cities_data_cli.py
@@ -4,6 +4,10 @@
 import sys
 import cities_data_access as cda
 
+from sqlalchemy import create_engine, text
+
+cda.DB_URI = 'sqlite:///../../../../data/world_db/world.db'
+
 def show_usage():
     print ("""
 cities_data_cli.py add city_name country_code district population
@@ -42,7 +46,7 @@ def main():
     elif method == 'update_population':
         count = update_city_population(sys.argv[2], int(sys.argv[3]))
     else:  # method == 'get_country_codes'
-        show_country_codes()
+        get_country_codes()
     
 
 def add_city(city_info):
@@ -51,13 +55,13 @@ def add_city(city_info):
     Args: 
         list: city_info (i.e. name, country code, district, population) 
     """
-    country_code = city_info[2]
+    country_code = city_info[1]
     if not is_valid_country_code(country_code):
         print(f'{country_code} is not a valid country code')
         print('Use get_country_codes to see a list of valid country codes\n')
         show_usage()
 
-    city_info = city_info[1:]
+    city_info = city_info[0:]
     id = cda.add_city(*city_info)
     if id == None:
         print('Unable to add city')
@@ -73,8 +77,12 @@ def get_city(name):
     Returns:
         tuple: (Name, country code, district, population)
     """
-    city_info = ''
-    return city_info
+    city_info = cda.get_city_by_name(name)
+    if city_info == None:
+        return "Oops, it doesn't exist in the city table."
+    else:
+        return city_info
+
 
 
 def delete_city(name):
@@ -83,7 +91,11 @@ def delete_city(name):
     Args:
         name (str): Name of the city to delete
     """
-
+    rowcount = cda.delete_city_by_name(name)
+    if rowcount >= 1:
+        print(f"{name} has been deleted from city table.")
+    else:
+        print(f"{name} doesn't exist in city table.")
 
 def update_city_population(name, population):
     """ Update the population value for a city
@@ -92,14 +104,19 @@ def update_city_population(name, population):
         name (str): Name of the city to update
         population (int): New population value
     """
+    rowcount = cda.update_city_population(name, population)
+    if rowcount >= 1:
+        print(f"The population of {name} has been updated.")
+    else:
+        print(f"{name} doesn't exist in city table.")
 
-def show_country_codes():
+def get_country_codes():
     """ Print the country codes and names to the console.
     """
     codes_and_names = cda.get_country_codes_and_names()
     for code, name in codes_and_names:
         print (code, name)
-
+    return codes_and_names
 
 def is_valid_country_code(code_to_check):
     """ Ensure the code_to_check is valid.
@@ -113,8 +130,13 @@ def is_valid_country_code(code_to_check):
 
     #TODO: Add the necessary code to determine if code_to_check is a
     #      valid country code or not
-    return False
-
+    cc_name = get_country_codes()
+    country_codes = {row[0] for row in cc_name}
+    if code_to_check not in country_codes:
+        return False
+    else:
+        return True
+    
 
 if __name__ == '__main__':
     main()
